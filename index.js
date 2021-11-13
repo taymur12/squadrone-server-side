2e88814599c455abb0d872cf4aa47478a74c74bc
Taymur <htaymur12@gmail.com>
2021-11-13 18:20:17 +0600

    added manage

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ async function run() {
         const order = database.collection("order")
         const users = database.collection("users")
         const review = database.collection("review")
+        const newusers = database.collection("newusers")
+        const addproduct = database.collection("addproduct")
 
         //get products
         app.get('/products', async (req, res) => {
@@ -82,6 +84,59 @@ async function run() {
             const cursor = review.find({})
             const reviewOrder = await cursor.toArray();
             res.json(reviewOrder)
+        })
+
+        //admin email set
+        app.get('/newusers/:email', async(req, res)=>{
+            const email = req.params.email;
+            const query = { email: email}
+            const user = await newusers.findOne(query)
+            let isAdmin = false;
+            if(user.role === 'admin'){
+                isAdmin = true
+            }
+            res.json({admin: isAdmin})
+        })
+
+
+        // post users
+        app.post('/newusers', async(req, res)=>{
+            const user = req.body
+            const result = await newusers.insertOne(user)
+            console.log(result)
+            res.json(result)
+        })
+
+        app.put('/newusers', async(req, res)=>{
+            const user = req.body
+            const query = {email: user.email}
+            const options = { upsert: true}
+            const updateDoc = {$set:user}
+            const result = await newusers.updateOne(query, updateDoc, options)
+            res.json(result)
+        })
+
+        app.put('/newusers/admin', async( req, res)=>{
+            const user = req.body
+            const filter = {email: user.email}
+            const updatedoc = {$set: {role:'admin'}};
+            const result = await newusers.updateOne(filter, updatedoc)
+            res.json(result)
+
+        })
+
+         //post added product
+         app.post('/addnewproduct', async (req, res) => {
+            const doc = req.body
+            const result = await addproduct.insertOne(doc)
+            res.json(result)
+        })
+
+        // // get added product
+        app.get('/addnewproducts', async (req, res) => {
+            const cursor = addproduct.find({})
+            const addedproduct = await cursor.toArray();
+            res.json(addedproduct)
         })
     }
     finally {
